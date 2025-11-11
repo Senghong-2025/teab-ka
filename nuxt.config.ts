972ae552cf2e0981057fea599440826b27a8d11a
SenghongH <senghong.hang@techbodia.com>
2025-11-11 20:40:55 +0700

    init tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['./app/assets/style/main.css'],
   runtimeConfig: {
     public: {
       NODE_ENV: import.meta.env.NODE_ENV,
       API_KEY: import.meta.env.API_KEY
     }
   },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
