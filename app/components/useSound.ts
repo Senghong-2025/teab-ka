e04614467ace651604d732346b14fdd3d323947d
SenghongH <senghong.hang@techbodia.com>
2025-11-14 16:02:43 +0700

    add sound

--- app/components/useSound.ts
+++ app/components/useSound.ts
@@ -1,7 +1,8 @@
 export default function useSound() {
     const soundList: Record<number, string> = {
         1: '/sound/1.mp3',
-        2: '/sound/2.mp3'
+        2: '/sound/2.mp3',
+        3: '/sound/3.mp3'
     }
     return {
         soundList,
