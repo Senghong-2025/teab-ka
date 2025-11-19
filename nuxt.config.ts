05d069a92e0701fe4d8164a54ba159896bc9fe99
SenghongH <senghong.hang@techbodia.com>
2025-11-19 23:35:57 +0700

    update SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: true,
+  nitro: {
+    preset: 'cloudflare-pages'
+  },
   css: [
     './app/assets/style/main.css',
     './app/assets/style/base.css'
